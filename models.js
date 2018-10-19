2cc0fbf0986a70a945c80d0025ce9d15c9ea0b5d
esokoletsky <Esokoletsky@gmail.com>
2018-10-18 23:55:33 -0400

    added schema

--- models.js
+++ models.js
@@ -3,4 +3,32 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-let 
+const routineSchema = mongoose.Schema(
+{
+    userName: {
+        type: "string",
+        unique: true
+    },
+    id: ObjectId,
+    day: "string",
+    muscleGroup: "string",
+    muscle: "string",
+    targetExercise: {
+    weight: number,
+    sets: number,
+    reps: number
+    }
+});
+
+routineSchema.methods.serialize = function() {
+    return {
+      id: this._id,
+      day: this.day,
+      muscleGroup: this.muscleGroup,
+      targetExercise: this.targetExercise  
+    };
+    };
+
+const Routine = mongoose.model('Routine', routineSchema);
+
+module.exports = {Routine};
